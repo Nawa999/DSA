461c30a5c5d2b9e4a36789bd38e278145f6ceeee
Nawa999 <nawanjanawickramasingha5@gmail.com>
2026-01-17 11:02:24 +0530

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package BinarySearchTree;
+import java.util.Scanner;
+
+
+public class BinarySearchTree {
+
+    class Node {
+        int key;
+        Node left, right;
+
+        Node(int key) {
+            this.key = key;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    // Insert
+    Node insert(Node root, int key) {
+        if (root == null) {
+            return new Node(key);
+        }
+
+        if (key < root.key)
+            root.left = insert(root.left, key);
+        else if (key > root.key)
+            root.right = insert(root.right, key);
+
+        return root;
+    }
+
+    // Inorder Traversal
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.key + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.root = bst.insert(bst.root, 50);
+        bst.insert(bst.root, 30);
+        bst.insert(bst.root, 70);
+        bst.insert(bst.root, 20);
+        bst.insert(bst.root, 40);
+        bst.insert(bst.root, 60);
+        bst.insert(bst.root, 80);
+
+        System.out.print("Inorder Traversal: ");
+        bst.inorder(bst.root);
+    }
+}
